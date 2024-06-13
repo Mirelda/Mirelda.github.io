3cd8b06bae312178a47da39b994749c5137d7dd4
Mirelda <mireldadiker@gmail.com>
2024-06-13 00:44:34 -0400

    Try to create portfolio

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.getElementById('resume').onclick = function() {
+    window.location.href = 'https://drive.google.com/file/d/1o2GjB_0FYPTFJe6fmM3OYnmd5m38k2XM/view?usp=drive_link'; 
+};
+
+document.getElementById('github').onclick = function() {
+    window.location.href = 'https://github.com/Mirelda'; 
+};
+
+document.getElementById('linkedin').onclick = function() {
+    window.location.href = 'https://www.linkedin.com/in/mirelda-diker/'; 
+};
+
+document.getElementById('contact').onclick = function() {
+    window.location.href = 'mailto:mireldadiker@gmail.com'; 
+};
